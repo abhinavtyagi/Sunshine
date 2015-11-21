f619d9600f029145811dae1efe3a6b9c896dcdfb
abhinav <abhityagi85@gmail.com>
2015-11-22 04:12:57 +0530

    Don't add fragment after serContentView as it will already instantiate fragment as per activity file otherwise fragment creationand menu inflate would happen twice!

--- app/src/main/java/aktyagi/com/sunshine/DetailActivity.java
+++ app/src/main/java/aktyagi/com/sunshine/DetailActivity.java
@@ -19,9 +19,6 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        if(savedInstanceState==null){
-            getSupportFragmentManager().beginTransaction().add(R.id.fragment, new DetailActivityFragment()).commit();
-        }
     }
 
 
